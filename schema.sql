e78b99650da44dcda83fbcd7784dbde71bf642fb
Mark Nguyen <101272613+markwin89@users.noreply.github.com>
2022-04-26 14:05:04 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -17,12 +17,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL, 
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -31,18 +31,18 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR (4) NOT NULL,	
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL, 
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,8 +50,5 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)	
-);
-	
-SELECT * FROM departments;	
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);	
